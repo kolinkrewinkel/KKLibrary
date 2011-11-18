35111217ef28a7e31d9e4c2a42d8d248bce4d1eb
Kolin Krewinkel <kolin.krewinkel@me.com>
2011-11-17 19:06:56 -0800

    Initial Commit

---
+++ KKLibrary/KKLibrary.h
@@ -0,0 +1,13 @@
+//
+//  KKLibrary.h
+//  KKLibrary
+//
+//  Created by Kolin Krewinkel on 11.17.11.
+//  Copyright (c) 2011 Enkla. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KKLibrary : NSObject
+
+@end
---
+++ KKLibrary/KKLibrary.m
@@ -0,0 +1,23 @@
+//
+//  KKLibrary.m
+//  KKLibrary
+//
+//  Created by Kolin Krewinkel on 11.17.11.
+//  Copyright (c) 2011 Enkla. All rights reserved.
+//
+
+#import "KKLibrary.h"
+
+@implementation KKLibrary
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
