6234aaf710614032548a29f2717f97fefbc41e1e
Kolin Krewinkel <kolin.krewinkel@me.com>
2011-11-17 20:06:23 -0800

    Rebuilt project.

--- KKLibrary/KKLibrary.m
+++ KKLibrary/KKLibrary.m
@@ -3,7 +3,7 @@
 //  KKLibrary
 //
 //  Created by Kolin Krewinkel on 11.17.11.
-//  Copyright (c) 2011 Enkla. All rights reserved.
+//  Copyright (c) 2011 Kolin Krewinkel. All rights reserved.
 //
 
 #import "KKLibrary.h"
